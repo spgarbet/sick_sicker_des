292f305148348aa0d592becdd02af18901f935a0
Shawn Garbett <shawn@garbett.org>
2025-01-23 14:39:00 -0600

    Added qmd document to summarize progress

--- event_death.R
+++ event_death.R
@@ -1,5 +1,17 @@
+  #############################################################################
+ #
 #
-
+# Copyright 2015, 2025 Shawn Garbett, Vanderbilt University Medical Center
+#
+# Permission to use, copy, modify, distribute, and sell this software and
+# its documentation for any purpose is hereby granted without fee,
+# provided that the above copyright notice appear in all copies and that
+# both that copyright notice and this permission notice appear in
+# supporting documentation. No representations are made about the
+# suitability of this software for any purpose.  It is provided "as is"
+# without express or implied warranty.
+#
+###############################################################################
 
 # Given attributes of a patient (trajectory), it returns in years 
 # how long till the patient would die a secular death.
